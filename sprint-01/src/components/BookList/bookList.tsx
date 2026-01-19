f14b87e313677cecd85ed0ce7de0f273cbd152aa
Gaganpreet Kaur <gkaur21@academic.rrc.ca>
2026-01-19 16:41:47 -0600

    Added my component.

---
+++ sprint-01/src/components/BookList/bookList.tsx
@@ -0,0 +1,18 @@
+import "./bookList.css"
+function BookList() {
+  return (
+    <section className="book-list">
+      <h2>Available Books in Library:</h2>
+
+      <ul>
+        <li>The Marrow Thieves</li>
+        <li>Good Habits</li>
+        <li>Harry Potter</li>
+        <li>Rich Dad Poor Dad</li>
+        <li>Cindrella</li>
+      </ul>
+    </section>
+  );
+}
+
+export default BookList;
