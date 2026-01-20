eda20bddfa033016f4c2018fc6052e978ea0cfb7
Gaganpreet Kaur <gkaur21@academic.rrc.ca>
2026-01-20 15:42:57 -0600

    Refactor App component structure and imports

--- sprint-01/src/App.tsx
+++ sprint-01/src/App.tsx
@@ -1,34 +1,26 @@
 import './App.css';
 import SearchFilter from './components/SearchFilter/SearchFilter';
-import './App.css'
-import LibraryTips from "./components/LibraryTips/LibraryTips";
+import LibraryTips from './components/LibraryTips/LibraryTips';
+import BookList from './components/BookList/bookList';
 
-import BookList from './components/BookList/bookList'
-import LibraryTips from "./Components/LibraryTips/LibraryTips";
 function App() {
   return (
     <div className="app">
-      <SearchFilter />
-    </div>
-  )
-}
-
-export default App
-  return (
-    <>
-    <BookList />
-
-    <header>
+      <header>
         <h1>Library Management App</h1>
       </header>
 
+      <SearchFilter />
+      <BookList />
       <LibraryTips />
 
       <footer>
-        <p>Gaganpreet Kaur, Harmanpreet Kaur, Manjot Kaur, Parneet Kaur</p>
+        <p>
+          Gaganpreet Kaur, Harmanpreet Kaur, Manjot Kaur, Parneet Kaur
+        </p>
       </footer>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App;
