2c831df656c4fcd509431cb6065e3de16af5bca5
Gaganpreet Kaur <gkaur21@academic.rrc.ca>
2026-02-03 15:34:52 -0600

    Added my shared state component.

--- sprint-01/src/App.tsx
+++ sprint-01/src/App.tsx
@@ -3,13 +3,7 @@ import BookList from "./Components/BookList/bookList";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "./Components/common/layout/Layout";
 import { useState } from "react";
-<<<<<<< HEAD
-import Home from "./Components/pages/Home";
-=======
-import Home from './Components/pages/Home';
-import { Layout } from './Components/common/layout/layout';
-import SearchFilter from "./Components/SearchFilter/SearchFilter";
->>>>>>> 1e0e488a9cf190edfa819827f2a023036180e9e7
+import {Home }from "../src/Components/pages/Home";
 
 function App() {
   const [books, setBooks] = useState([
@@ -18,27 +12,18 @@ function App() {
     { id: 3, title: "Harry Potter" },
   ]);
 
-  const [search, setSearch] = useState("");
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+          <Route index element={<Home books={books} setBooks={setBooks} />} />
+
+          
 
           <Route
             path="booklist"
-            element={
-              <BookList
-                books={books}
-                setBooks={setBooks}
-                search={search}
-                setSearch={setSearch}
-              />
-            }
+            element={<BookList books={books} setBooks={setBooks} />}
           />
-          <Route path="searchFilter" element={<SearchFilter />} />
-          <Route path="booklist" element={<BookList />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- sprint-01/src/Components/pages/SearchFilterPage.tsx
+++
@@ -1,7 +0,0 @@
-import SearchFilter from "./SearchFilter/SearchFilter";
-
-export const SearchFilterPage = () => {
-  return <SearchFilter />;
-}
-
-export default SearchFilterPage;
