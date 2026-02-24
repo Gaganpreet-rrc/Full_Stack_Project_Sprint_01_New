cd50f9c5f371bdbd4f081b37fb5ca7e23d94f1b2
Manjot Kaur <manjot21104@gmail.com>
2026-02-24 16:34:57 -0600

    Added Login component with repository and test data

---
+++ sprint-01/src/repositories/LoginRepo.ts
@@ -0,0 +1,42 @@
+import { LoginUser } from "../types/LoginUser";
+
+// Test data for login
+let testLoginUsers: LoginUser[] = [
+  { id: 1, username: "gaganpreet", password: "1234" },
+  { id: 2, username: "manjot", password: "abcd" },
+];
+
+export class LoginRepository {
+  // Get all users
+  getAll(): LoginUser[] {
+    return testLoginUsers;
+  }
+
+  // Get user by username 
+  getByUsername(username: string): LoginUser | undefined {
+    return testLoginUsers.find(user => user.username === username);
+  }
+
+  // Create a new login user
+  create(user: LoginUser): LoginUser {
+    testLoginUsers.push(user);
+    return user;
+  }
+
+  // Update user information
+  update(id: number, updatedUser: Partial<LoginUser>): LoginUser | undefined {
+    const user = testLoginUsers.find(u => u.id === id);
+    if (user) {
+      Object.assign(user, updatedUser);
+      return user;
+    }
+    return undefined;
+  }
+
+  // Delete a user
+  delete(id: number): boolean {
+    const beforeDeleteCount = testLoginUsers.length; // simple name
+    testLoginUsers = testLoginUsers.filter(user => user.id !== id);
+    return testLoginUsers.length < beforeDeleteCount;
+  }
+}
---
+++ sprint-01/src/types/LoginUser.ts
@@ -0,0 +1,6 @@
+export type LoginUser = {
+  id: number;
+  username: string;
+  password: string; 
+  token?: string;   
+};
