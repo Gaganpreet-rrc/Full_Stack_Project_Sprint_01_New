681116ffd8d3233dc1c7140d75677d88cfbdef8d
Manjot Kaur <manjot21104@gmail.com>
2026-03-03 21:55:02 -0600

    update the files

--- sprint-01/src/Components/pages/BookListPage.tsx
+++ sprint-01/src/Components/pages/BookListPage.tsx
@@ -1,32 +1,8 @@
 import React from "react";
+import { BookList } from "../BookList/bookList";
 import { useLibraryContext } from "../../context/LibraryContext";
-import { BookList } from "../BookList/BookList";
 import { searchService } from "../../services/searchfilterService";
  
-
-/**
- * BookListPage Component
- *
- * - Uses useLibraryContext hook for shared book state
- * - Shows BookList component
- */
-export const BookListPage: React.FC = () => {
-  const { books, search, isGridView, toggleView } = useLibraryContext();
-
-  // You can filter books here if needed
-  const filteredBooks = books.filter((book) =>
-    book.title.toLowerCase().includes(search.toLowerCase())
-  );
-
-  return (
-    <div>
-      <h1>Library Books</h1>
-      <BookList />
-    </div>
-  );
-};
-
-
  
 export const BookListPage: React.FC = () => {
   const { books, search, isGridView, toggleView } = useLibraryContext();
