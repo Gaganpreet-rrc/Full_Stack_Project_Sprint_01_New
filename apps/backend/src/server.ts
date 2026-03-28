51484bc312fceec656c4386d1e0dbca0fa0ef656
Gaganpreet Kaur <gkaur21@academic.rrc.ca>
2026-03-27 20:01:22 -0500

    Completed back-end resource endpoints by creating routes, services, controller and also tested postman to make sure its working as expected.

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,7 +1,14 @@
 import express from "express";
+import userRoutes from "./routes/userRoutes";
+import "dotenv/config";
+
 
 const app = express();
+app.use(express.json());
+
 const PORT = 3000;
+
+app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Backend is running!");
