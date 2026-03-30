1fb4bf726c9824917b21b6b70692778f2b43c585
Manjot Kaur <manjot21104@gmail.com>
2026-03-30 16:35:49 -0500

    Completed I.1 with add login endpoint with route-controller-service structure

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import authRoutes from "../src/routes/auth.routes"
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Backend is running!");
