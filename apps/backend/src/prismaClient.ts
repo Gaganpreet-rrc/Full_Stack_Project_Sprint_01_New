98366dc62b4447e3e5af648ad066ab268fa341c1
Gaganpreet Kaur <gkaur21@academic.rrc.ca>
2026-03-31 09:49:30 -0500

    Refactor prismaClient.ts to remove duplicates
    Removed duplicate initialization of Prisma client.

--- apps/backend/src/prismaClient.ts
+++ apps/backend/src/prismaClient.ts
@@ -7,10 +7,3 @@ const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
  
 export { prisma };
-const connectionString = `${process.env.DATABASE_URL}`;
-
-
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
-
-export { prisma };
